d9572e6ee15dfcd762b904112ec57dca2b00a5e9
DamianP5 <damian.piriz@correo.ucu.edu.uy>
2022-09-13 14:20:32 -0300

    Hecho el ejercicio 1

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,8 +9,12 @@ namespace SRP
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            //Como hicimos una nueva clase con un nuevo constructor, el codigo anterior para "ShelveBook" no funciona, entonces ingresamos el siguiente:
+            ShelveBook Shelve1 = new ShelveBook("A", "7");
+            ShelveBook Shelve2 = new ShelveBook("B", "3");
+
+            /*book1.ShelveBook("A","7");
+            book2.ShelveBook("B","3");*/
 
         }
     }
